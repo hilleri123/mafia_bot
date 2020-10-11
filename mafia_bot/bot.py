7797445d56a158afad1adb36b60f8865578eebb7
shurik <shurik@skotch.nirvana>
2020-10-11 19:02:52 +0300

    somethings works

--- mafia_bot/bot.py
+++ mafia_bot/bot.py
@@ -4,14 +4,49 @@ import discord
 from discord.ext import commands
 from config import settings
 
+import random
+
 
 bot = commands.Bot(command_prefix = settings['prefix'])
 
 
 @bot.command()
-async def hello(ctx):
-    author = ctx.message.author
+async def stupid_help(ctx):
+    await ctx.send(
+            'help - для хелпы, очевидно.\n',
+            'check [civ, maf, com=1, don=1] - чтоб разослать роди, тупица.\n'
+            )
 
-    await ctx.send(f'Hello, {author.mention}')
+
+
+text_tokens = ['Мирный', 'Мафия', 'Комиссар', 'Дон мафии']
+
+
+@bot.command()
+async def check(ctx, *args):
+    roles = []
+    print(args)
+
+    for i, role in enumerate(text_tokens):
+        if i < len(args):
+            roles.extend([role]*int(args[i]))
+        else:
+            roles.append(role)
+
+    random.shuffle(roles)
+    print(roles)
+
+    author = ctx.author
+    channel = author.voice.channel
+    print(author, channel.members)
+    for i, member in enumerate(channel.members):
+        member_role = roles[i]
+        print(i, member.mention, member_role)
+        await member.send(f'{author.mention} loshara, ti est {member_role}')
+
+
+
+
+
 
 
--- mafia_bot/config.py
+++ mafia_bot/config.py
@@ -1,8 +1,18 @@
 
+#токен старый и уже не работает
+token = 'NzY0ODU5NTM0NTY0MTMwODY4.X4MY8w.kuJqUAb6KhNIVXmY9qQx_e5h3so'
+
+try:
+    with open('token.txt', 'r') as token_file:
+        token = token_file.readline()
+except FileNotFoundError:
+    with open('token.txt', 'a') as token_file:
+        token_file.close()
+
+
 
 client_id = '764859534564130868'
 bot_name = 'AJlKO3ABPUK'
-token = 'NzY0ODU5NTM0NTY0MTMwODY4.X4MY8w.kuJqUAb6KhNIVXmY9qQx_e5h3so'
 link = 'https://discordapp.com/oauth2/authorize?&client_id='+client_id+'&scope=bot&permissions=8'
 
 
@@ -10,6 +20,7 @@ settings = {
         'token' : token, 
         'bot' : bot_name,
         'id' : int(client_id),
-        'prefix' : '4JlEH'
+        #'prefix' : '4JlEH '
+        'prefix' : '!'
         }
 
