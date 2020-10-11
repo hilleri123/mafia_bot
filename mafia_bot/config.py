1fb31b8a26c561aa49cfa552ddd08cbe3b873d9a
shurik <shurik@skotch.nirvana>
2020-10-11 18:08:30 +0300

    first

---
+++ mafia_bot/config.py
@@ -0,0 +1,15 @@
+
+
+client_id = '764859534564130868'
+bot_name = 'AJlKO3ABPUK'
+token = 'NzY0ODU5NTM0NTY0MTMwODY4.X4MY8w.kuJqUAb6KhNIVXmY9qQx_e5h3so'
+link = 'https://discordapp.com/oauth2/authorize?&client_id='+client_id+'&scope=bot&permissions=8'
+
+
+settings = {
+        'token' : token, 
+        'bot' : bot_name,
+        'id' : int(client_id),
+        'prefix' : '4JlEH'
+        }
+
---
+++ mafia_bot/run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+from bot import bot
+from config import settings
+
+bot.run(settings['token'])
+
